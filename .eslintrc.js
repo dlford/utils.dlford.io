be33199d2ea46a85861f860e2dd06544970d0576
dlford <dan-github2@dlford.co>
2022-01-15 18:19:24 -0700

    initial config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: false,
+  },
+  plugins: ['prettier', 'react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+  },
+  overrides: [],
+};
--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    /* ... */
+    public: '/',
+    src: '/dist',
   },
   plugins: [
-    /* ... */
+    '@snowpack/plugin-react-refresh',
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
