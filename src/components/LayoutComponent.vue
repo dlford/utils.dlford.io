d79b653e5ae51d07d08312ead4d6301512911e23
dlford <dan-github2@dlford.co>
2022-02-25 13:27:19 -0700

    basic structure for Vue rewrite

--- src/components/LayoutComponent.vue
+++ src/components/LayoutComponent.vue
@@ -1,13 +1,49 @@
+<script>
+import tools from '../tools';
+
+export default {
+  data: function () {
+    return {
+      currentTool: tools[0],
+      tools,
+    };
+  },
+  methods: {
+    changeTool(tool) {
+      this.currentTool = tool;
+    },
+  },
+};
+</script>
+
 <template>
-  <div class="header"> WIP: Vue rewrite in progress </div>
+  <div class="outer">
+    <div class="nav">
+      <ul>
+        <li
+          v-for="tool in tools"
+          :key="tool.title"
+          @click="changeTool(tool)"
+        >
+          {{ tool.title }}
+        </li>
+      </ul>
+    </div>
+    <div class="tool">
+      <keep-alive>
+        <component :is="currentTool.payload" />
+      </keep-alive>
+    </div>
+  </div>
 </template>
 
-<style>
-.header {
+<style scoped>
+.outer {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
   max-height: 100%;
+  gap: 2rem;
 }
 </style>
---
+++ src/tools/index.js
@@ -0,0 +1,13 @@
+import WelcomeTool from './WelcomeTool.vue';
+import TestTool from './TestTool.vue';
+
+export default [
+  {
+    title: 'Welcome',
+    payload: WelcomeTool,
+  },
+  {
+    title: 'Test',
+    payload: TestTool,
+  },
+];
